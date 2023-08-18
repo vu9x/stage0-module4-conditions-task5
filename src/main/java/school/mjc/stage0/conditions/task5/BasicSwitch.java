05b229be544c0cd1817612706bc09459ee1d32e4
Vu <nguyentuanvu9x@gmail.com>
2023-08-18 13:13:40 +0300

    Adding: BasicSwitch.java

--- src/main/java/school/mjc/stage0/conditions/task5/BasicSwitch.java
+++ src/main/java/school/mjc/stage0/conditions/task5/BasicSwitch.java
@@ -4,17 +4,17 @@ public class BasicSwitch {
     public static void main(String[] args) {
         int a = 20;
         switch (a) {
+            case 7:
+//                System.out.println("seven");
+//                break;
+            case 10:
+//                System.out.println("ten");
+//                break;
+            case 20:
+//                System.out.println("twenty");
+//                break;
             default:
                 System.out.println("default");
-                break;
-            case 7:
-                System.out.println("seven");
-                break;
-            case 10:
-                System.out.println("ten");
-                break;
-            case 20:
-                System.out.println("twenty");
                 break;
         }
     }
